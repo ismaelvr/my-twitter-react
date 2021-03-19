b32009488b4369486d9342a9990402108ceccac4
Ismael Velez Recio <ismael.velez@alu.uclm.es>
2021-03-19 21:00:50 +0100

    Version 1.0.1 Fin sesion 3

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { HashRouter, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+//import Home from './components/Home';
+import 'bootstrap/dist/css/bootstrap.css';
+import App from './components/App';
+import './index.css';
+ReactDOM.render(<HashRouter>
+      <div>
+        <Route exact path="/" component={App} />
+      </div>
+</HashRouter >, document.getElementById('root'));
+   
+  //  <Route path="/home" component={Home} />
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
